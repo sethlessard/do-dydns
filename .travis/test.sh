96330dd3b19476ff78dde7e944aaab994a6aaf90
Seth Lessard <sethlessard@outlook.com>
2020-10-10 09:54:24 -0500

    travis should fail if the api has an error

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,11 +3,16 @@ set -ex
 
 # make sure the frontend container was created (do-dydns_frontend)
 docker ps | grep do-dydns_frontend
-docker logs do-dydns_frontend_1
+docker logs do-dydns_frontend_1 > frontend.log
+cat frontend.log
 
 # make sure the api container was created (do-dydns_api)
 docker ps | grep do-dydns_api
-docker logs do-dydns_api_1
+docker logs do-dydns_api_1 > api.log
+cat api.log
+
+$apiLog = cat api.log
+if [[ "$apiLog" != "Api listening at: 0.0.0.0:3080" ]]; then exit 1;
 
 # make sure the api was started and accessible
 curl localhost:3080/
