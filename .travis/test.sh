6e112655f20758bc97e855cdaedf4453775e7a4c
Seth Lessard <sethlessard@outlook.com>
2020-10-10 09:35:10 -0500

    fix issue printing docker logs during travis build

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,11 +3,11 @@ set -ex
 
 # make sure the frontend container was created (do-dydns_frontend)
 docker ps | grep do-dydns_frontend
-docker logs do-dydns_frontend
+docker logs do-dydns_frontend_1
 
 # make sure the api container was created (do-dydns_api)
 docker ps | grep do-dydns_api
-docker logs do-dydns_api
+docker logs do-dydns_api_1
 
 # make sure the api was started and accessible
 curl localhost:3080/
