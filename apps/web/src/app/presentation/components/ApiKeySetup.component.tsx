083e78a9bf14aeb5547e38e88c30f68c4651a990
Seth Lessard <sethlessard@outlook.com>
2021-04-04 12:10:22 -0500

    feat(web): added initial setup instructions

--- apps/web/src/app/presentation/components/ApiKeySetup.component.tsx
+++ apps/web/src/app/presentation/components/ApiKeySetup.component.tsx
@@ -6,9 +6,12 @@ import {
   CardHeader,
   createStyles,
   Grid,
+  List,
+  ListItem,
   makeStyles,
   Theme,
   Tooltip,
+  Typography,
 } from "@material-ui/core";
 import { Info as InfoIcon } from "@material-ui/icons";
 import { Link } from "react-router-dom";
@@ -40,7 +43,56 @@ export function ApiKeySetup() {
             </Tooltip>
           }
         />
-        <CardContent>TODO: List the steps to get started</CardContent>
+        <CardContent>
+          <Typography>Prerequisites</Typography>
+          <List>
+            <ListItem>
+              1. Create an account on &nbsp;
+              <a href={"https://digitalocean.com"} target={"__blank"}>
+                Digital Ocean
+              </a>
+              .
+            </ListItem>
+            <ListItem>2. Have a domain.</ListItem>
+            <ListItem>
+              3.&nbsp;
+              <a
+                href={
+                  "https://www.digitalocean.com/community/tutorials/how-to-point-to-digitalocean-nameservers-from-common-domain-registrars"
+                }
+                target={"__blank"}
+              >
+                Register your domain to use Digital Ocean's nameservers.
+              </a>
+            </ListItem>
+            <ListItem>
+              4.&nbsp;
+              <a
+                href={
+                  "https://www.digitalocean.com/docs/networking/dns/how-to/add-domains/"
+                }
+                target={"__blank"}
+              >
+                Add your domain to your Digital Ocean account.
+              </a>
+            </ListItem>
+            <ListItem>
+              5.&nbsp;
+              <a
+                href={
+                  "https://www.digitalocean.com/docs/apis-clis/api/create-personal-access-token/"
+                }
+                target={"__blank"}
+              >
+                Create your Digital Ocean API key.
+              </a>
+            </ListItem>
+            <ListItem>
+              6.&nbsp;<Link to={"/settings"}>Add</Link>&nbsp;your API key to
+              DO-DyDns
+            </ListItem>
+          </List>
+        </CardContent>
         <CardActions>
           <div className={classes.flexGrow} />
           <a
@@ -49,7 +101,6 @@ export function ApiKeySetup() {
             }
             target={"__blank"}
           >
-            {" "}
             <Button color={"secondary"} variant={"outlined"}>
               How to Create an API Key
             </Button>
